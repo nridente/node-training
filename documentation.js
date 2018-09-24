b0c51b98c4ad39c86b0565428059ebec1dd6b88d
Nicol Ridente <nicol.ridente@wolox.com.ar>
2018-09-24 13:25:13 -0300

    add documentation file and documented user endpoint

---
+++ documentation.js
@@ -0,0 +1,19 @@
+var dictum = require('dictum.js');
+
+dictum.document({
+  description: 'Some description for the given endpoint',
+  endpoint: '/users',
+  method: 'POST',
+  requestHeaders: { 'Content-Type': 'application/json' },
+  requestPathParams: { /* path params for endpoint */ },
+  requestBodyParams: { 
+    email: 'email@wolox.com.ar',
+    name: 'Valdomero',
+    last_name: 'Lord',
+    password: 'password'
+  },
+  responseStatus: 200,
+  responseHeaders: { /* headers for response */ },
+  responseBody: { /* body params for response */ },
+  resource: 'My Resource'
+});
